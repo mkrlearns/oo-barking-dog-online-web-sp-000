ef8e39b0add3c8ba0a360bc09b3aa9f53497a91e
ryan m <contact@ryanmeek.com>
2019-01-25 23:51:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -18,17 +18,3 @@ class Dog
   end
   
 end
-
-class Dog
-  def initialize(breed)
-    @breed = breed
-  end
- 
-  def breed=(breed)
-    @breed = breed
-  end
- 
-  def breed
-    @breed
-  end
-end
